0f565832ab8003b8d1716ffe8a166ee182c5f8a5
chelito17 <156563040+chelito17@users.noreply.github.com>
2024-01-19 15:06:17 +0000

    Add files via upload

---
+++ script_integrantes.py
@@ -0,0 +1,9 @@
+def imprimir_integrantes():
+    integrantes = ["Marcelo Nunez", "Joseph Pancetti"]
+    
+    print("Integrantes del grupo:")
+    for integrante in integrantes:
+        print("-", integrante)
+
+if __name__ == "__main__":
+    imprimir_integrantes()
